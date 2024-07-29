49e534336a02274d19d1871f8a11627deec81f1b
Lucas Israel <lisraelalves92@gmail.com>
2024-07-29 17:59:37 -0300

    ref: testing changes to RecipeViewModel

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/recipe/ui/viewmodels/RecipeViewModel.kt
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/recipe/ui/viewmodels/RecipeViewModel.kt
@@ -16,24 +16,44 @@ import javax.inject.Inject
 class RecipeViewModel @Inject constructor(private val recipeRepository: RecipeRepository) :
     ViewModel() {
 
-    private val _meals = MutableLiveData<Meals>()
-    val meals: MutableLiveData<Meals>
-        get() = _meals
-
-    private val _fetchError = MutableStateFlow<Boolean>(false)
+    private val _fetchError = MutableStateFlow(false)
     val fetchError: StateFlow<Boolean>
         get() = _fetchError
+
+    private val _recipeName = MutableLiveData<String>()
+    val recipeName: MutableLiveData<String>
+        get() = _recipeName
+
+    private val _categoryName = MutableLiveData<String>()
+    val categoryName: MutableLiveData<String>
+        get() = _categoryName
+
+    private val _recipeArea = MutableLiveData<String>()
+    val recipeArea: MutableLiveData<String>
+        get() = _recipeArea
+
+    private val _tags = MutableLiveData<String>()
+    val tags: MutableLiveData<String>
+        get() = _tags
 
     fun getRecipe(id: String) {
         viewModelScope.launch {
             try {
                 val response = recipeRepository.getRecipe(id)
-                _meals.postValue(response)
                 _fetchError.value = false
+                updatingRecipeResponse(response)
             } catch (e: ConnectException) {
                 _fetchError.value = true
             }
         }
+    }
+
+    private fun updatingRecipeResponse(response: Meals) {
+        val recipe = response.meals[0]
+        _recipeName.postValue(recipe.strMeal)
+        _categoryName.postValue(recipe.strCategory)
+        _recipeArea.postValue(recipe.strArea)
+        _tags.postValue(recipe.strTags)
     }
 
 }
