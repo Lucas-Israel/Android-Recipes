c6b30a37300c9eae213581535f3dbd497223ebe5
Lucas Israel <lisraelalves92@gmail.com>
2024-07-29 14:11:38 -0300

    removed unused import

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/category/data/api/CategoryApiService.kt
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/category/data/api/CategoryApiService.kt
@@ -3,7 +3,6 @@ package br.com.lucasIsrael.androidrecipes.meals.category.data.api
 import br.com.lucasIsrael.androidrecipes.meals.category.data.model.Meals
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CategoryApiService {
