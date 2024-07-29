f14c8a24859fb22618d88e4e0d5b518ad1d41ac1
Lucas Israel <lisraelalves92@gmail.com>
2024-07-29 18:51:39 -0300

    ref: changed RecipeViewModel to only a Recipe

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/recipe/ui/viewmodels/RecipeViewModel.kt
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/recipe/ui/viewmodels/RecipeViewModel.kt
@@ -3,7 +3,7 @@ package br.com.lucasIsrael.androidrecipes.meals.recipe.ui.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.lucasIsrael.androidrecipes.meals.recipe.data.model.Meals
+import br.com.lucasIsrael.androidrecipes.meals.recipe.data.model.Recipe
 import br.com.lucasIsrael.androidrecipes.meals.recipe.data.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,40 +20,21 @@ class RecipeViewModel @Inject constructor(private val recipeRepository: RecipeRe
     val fetchError: StateFlow<Boolean>
         get() = _fetchError
 
-    private val _recipeName = MutableLiveData<String>()
-    val recipeName: MutableLiveData<String>
-        get() = _recipeName
-
-    private val _categoryName = MutableLiveData<String>()
-    val categoryName: MutableLiveData<String>
-        get() = _categoryName
-
-    private val _recipeArea = MutableLiveData<String>()
-    val recipeArea: MutableLiveData<String>
-        get() = _recipeArea
-
-    private val _tags = MutableLiveData<String>()
-    val tags: MutableLiveData<String>
-        get() = _tags
+    private val _recipe = MutableLiveData<Recipe>()
+    val recipe: MutableLiveData<Recipe>
+        get() = _recipe
 
     fun getRecipe(id: String) {
         viewModelScope.launch {
             try {
                 val response = recipeRepository.getRecipe(id)
+                val recipe = response.meals[0]
                 _fetchError.value = false
-                updatingRecipeResponse(response)
+                _recipe.postValue(recipe)
             } catch (e: ConnectException) {
                 _fetchError.value = true
             }
         }
-    }
-
-    private fun updatingRecipeResponse(response: Meals) {
-        val recipe = response.meals[0]
-        _recipeName.postValue(recipe.strMeal)
-        _categoryName.postValue(recipe.strCategory)
-        _recipeArea.postValue(recipe.strArea)
-        _tags.postValue(recipe.strTags)
     }
 
 }
