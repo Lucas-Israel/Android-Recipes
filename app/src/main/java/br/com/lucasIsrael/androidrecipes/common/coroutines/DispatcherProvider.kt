39600868f9faa07b82e01c4339dbde74defc1047
Lucas Israel <lisraelalves92@gmail.com>
2024-08-06 16:46:39 -0300

    removed unnecessary file

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/common/coroutines/DispatcherProvider.kt
+++
@@ -1,6 +0,0 @@
-package br.com.lucasIsrael.androidrecipes.common.coroutines
-
-import kotlinx.coroutines.Dispatchers
-import kotlin.coroutines.CoroutineContext
-
-object DispatcherProvider { val IO: CoroutineContext = Dispatchers.IO }
