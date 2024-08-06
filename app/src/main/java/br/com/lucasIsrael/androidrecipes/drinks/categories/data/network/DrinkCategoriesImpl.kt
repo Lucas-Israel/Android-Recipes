a39d113df23624b41d31e14419a93938cb5cf198
Lucas Israel <lisraelalves92@gmail.com>
2024-08-06 18:19:28 -0300

    DrinkCategoriesDataSource

---
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/drinks/categories/data/network/DrinkCategoriesImpl.kt
@@ -0,0 +1,13 @@
+package br.com.lucasIsrael.androidrecipes.drinks.categories.data.network
+
+import br.com.lucasIsrael.androidrecipes.drinks.categories.data.api.DrinkCategoriesApiService
+import br.com.lucasIsrael.androidrecipes.drinks.categories.data.model.DrinkCategories
+import javax.inject.Inject
+
+class DrinkCategoriesImpl @Inject constructor(private val apiService: DrinkCategoriesApiService) :
+    DrinkCategoriesDataSource {
+
+    override suspend fun getCategories(): DrinkCategories {
+        return apiService.getCategories()
+    }
+}
