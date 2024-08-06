17586b57328fc3a9f0a8456fd8b051c0c8609f0c
Lucas Israel <lisraelalves92@gmail.com>
2024-08-06 14:59:11 -0300

    removed Response<T>

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/category/data/api/CategoryApiService.kt
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/meals/category/data/api/CategoryApiService.kt
@@ -1,12 +1,11 @@
 package br.com.lucasIsrael.androidrecipes.meals.category.data.api
 
 import br.com.lucasIsrael.androidrecipes.meals.category.data.model.Meals
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CategoryApiService {
 
     @GET("filter.php")
-    suspend fun getCategory(@Query("c") category: String): Response<Meals>
+    suspend fun getCategory(@Query("c") category: String): Meals
 }
