5a0d3ae0ea71ddddc3ec6b4f95dcdb157d21f203
Lucas Israel <lisraelalves92@gmail.com>
2024-08-08 16:53:33 -0300

    feat: navigation from DrinkCategories to DrinkCategory

--- app/src/main/java/br/com/lucasIsrael/androidrecipes/drinks/categories/ui/adapter/DrinkCategoriesAdapter.kt
+++ app/src/main/java/br/com/lucasIsrael/androidrecipes/drinks/categories/ui/adapter/DrinkCategoriesAdapter.kt
@@ -2,10 +2,12 @@ package br.com.lucasIsrael.androidrecipes.drinks.categories.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import br.com.lucasIsrael.androidrecipes.databinding.ListItemDrinkCategoriesBinding
 import br.com.lucasIsrael.androidrecipes.drinks.categories.data.model.DrinkCategory
+import br.com.lucasIsrael.androidrecipes.drinks.categories.ui.fragment.DrinkCategoriesFragmentDirections
 
 class DrinkCategoriesAdapter(private val drinkCategories: List<DrinkCategory>) :
     RecyclerView.Adapter<DrinkCategoriesAdapter.DrinkCategoriesViewHolder>() {
@@ -32,6 +34,14 @@ class DrinkCategoriesAdapter(private val drinkCategories: List<DrinkCategory>) :
 
     override fun onBindViewHolder(holder: DrinkCategoriesViewHolder, position: Int) {
         val item = drinkCategories[position]
+
+        holder.itemView.setOnClickListener {
+            val action =
+                DrinkCategoriesFragmentDirections.actionDrinkCategoriesFragmentToDrinkCategoryFragment(
+                    item.strCategory
+                )
+            holder.itemView.findNavController().navigate(action)
+        }
 
         holder.bind(item)
     }
